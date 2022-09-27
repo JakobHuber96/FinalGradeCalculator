0bc981293dbf17251a4dfaa1d6361b5b99c9ecb4
Jakob Huber <93046@psdschools.org>
2022-09-27 10:50:15 -0600

    Map

--- FinalGradeCalculator/ViewController.swift
+++ FinalGradeCalculator/ViewController.swift
@@ -10,10 +10,66 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        finalSum.isHidden = true
         super.viewDidLoad()
+     //   finalResult = ""
         // Do any additional setup after loading the view.
     }
+    var testWorthDouble = 0.0
+    var currentGradeDouble = 0.0
+    var desiredGradeDouble = 0.0
+    var testWorthDouble2 = 0.0
+    var testWorthString = ""
+    var currentGradeString = ""
+    var desiredGradeString = ""
+    var currentWorth = 0.0
+    var currentContribution = 0.0
+    var 🥰 = 0.0
+    var 🙈🙈 = 0.0
+    var 🙈🙈string = ""
 
-
+    @IBOutlet var finalSum: UILabel!
+    
+    @IBOutlet var currentGrade: UITextField!
+    @IBOutlet var testWorth: UITextField!
+    @IBOutlet var desiredGrade: UITextField!
+    @IBAction func calculate(_ sender: Any) {
+        finalSum.isHidden = false
+        testWorthString = testWorth.text ?? ""
+        desiredGradeString = desiredGrade.text ?? ""
+        currentGradeString = currentGrade.text ?? ""
+     
+        currentGradeDouble = Double(currentGradeString) ?? 0.0
+ 
+        desiredGradeDouble = Double(desiredGradeString) ?? 0.0
+    
+        testWorthDouble = Double(testWorthString) ?? 0.0
+        
+      testWorthDouble2 = testWorthDouble / 100
+    
+       currentWorth = 1 - testWorthDouble2
+        
+        currentContribution = currentGradeDouble * currentWorth
+     
+        🥰 = desiredGradeDouble - currentContribution
+        
+        🙈🙈 = 🥰 / testWorthDouble2
+        
+        finalSum.text = "\(🙈🙈)"
+        
+        
+        
+        //   testWorthDouble2 = testWorthDouble / 100
+        
+        
+        
+    
+    }
+    
+    
+    
+    
+    
+    
+    
 }
-
